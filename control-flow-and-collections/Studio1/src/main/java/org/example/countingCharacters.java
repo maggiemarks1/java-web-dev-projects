685b65c305eeb9aa09f05958716aeec34dda9041
maggiemarks1 <maggiemarkus1@gmail.com>
2023-08-24 20:40:56 -0500

    studio 2

---
+++ control-flow-and-collections/Studio1/src/main/java/org/example/countingCharacters.java
@@ -0,0 +1,32 @@
+package org.example;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class countingCharacters {
+    public static void main(String[] args) {
+        String text = "If the product of two terms is zero then common sense says at least one of the two terms has to be zero to start with. So if you move all the terms over to one side, you can put the quadratics into a form that can be factored allowing that side of the equation to equal zero. Once you’ve done that, it’s pretty straightforward from there.";
+        char[] charactersInString = text.toCharArray();
+        HashMap<Character,Integer> letters = new HashMap<>();
+        for(char letter: charactersInString){
+          // System.out.println(letter);
+            if(letters.containsKey(letter)) {
+                letters.put(letter, letters.get(letter)+1);
+            } else {
+                letters.put(letter, 1);
+
+            }
+            }
+
+        for (Map.Entry<Character,Integer> letter : letters.entrySet()) {
+            System.out.println(letter.getKey() + " : " + letter.getValue() );
+        }
+
+    }
+//        for (int i =0; i< charactersInString.length; i++){
+//        System.out.println(charactersInString[i]);
+      //  }
+
+    }
+
+
--- datatypes/datatypes-studio/src/main/java/org/launchcode/Area.java
+++ datatypes/datatypes-studio/src/main/java/org/launchcode/Area.java
@@ -5,7 +5,7 @@ public class Area {
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
         System.out.println("Enter a Radius:");
-        double radius = input.nextInt();
+        double radius = input.nextDouble();
         double area = Circle.getArea(radius);
         System.out.println("The area of a circle of a radius of " + radius + " is " + area);
 
